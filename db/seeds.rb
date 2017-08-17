e8d373e854e0b6c6202b38b5143b8b9e51547270
Paul Sanford <sanfordpaul@gmail.com>
2017-08-16 22:35:30 -0500

    sign up, log in forms styled, db reseeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
+require 'faker'
 
 # Create Users
 5.times do
